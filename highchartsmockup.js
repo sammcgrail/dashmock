72abc63e440bd7533bf8de2572ff6dbbcc24cb72
sammcgrail <sammcgrail@gmail.com>
2016-09-20 15:10:02 -0400

    hc update

--- highchartsmockup.js
+++ highchartsmockup.js
@@ -1,7 +1,14 @@
+var dates = dateArray(50)
+var data1 = newDataArray(dates, randArrayMaker(50,5,80))
+var data2 = newDataArray(dates, randArrayMaker(50,5,123))
+var data3 = newDataArray(dates, randArrayMaker(50,5,20))
+
 $(function () {
-    $('#container').highcharts('StockChart',  {
+    $('#container').highcharts(  {
         chart: {
-            zoomType: 'xy'
+          panning: true,
+          pinchType: 'x',
+          inverted: false
         },
         // title: {
         //     text: 'High Stock Prime Mockup'
@@ -10,9 +17,27 @@ $(function () {
             text: 'Prime Random Test Data'
         },
         xAxis: [{
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            crosshair: true
+            // categories: dates,
+            // startOnTick: false, // only when navigator enabled
+            // endOnTick: false, // only when navigator enabled
+            // minPadding: 0,
+            // maxPadding: 0,
+            // ordinal: true,
+            // title: {
+            //     text: null
+            // },
+            // labels: {
+            //     overflow: 'justify'
+            // },
+            // showLastLabel: true,
+            type: 'datetime',
+            labels: {
+                formatter: function () {
+                  debugger
+                    return this.value + ' units';
+                }
+            }
+             // in Highstock only supported type
         }],
         yAxis: [{ // Primary yAxis
             labels: {
@@ -61,13 +86,34 @@ $(function () {
             opposite: true
         }],
         tooltip: {
-            shared: true
+            shared: true,
+            valueDecimals: 2
         },
         rangeSelector: {
             enabled: false
         },
         navigator: {
-            height: 40
+            height: 60,
+            enabled: true,
+      //       xAxis: {
+      //           categories: dates,
+      //           plotOptions: {
+      //             line : {
+      //               dataLabels : {
+      //                 enabled : true,
+      //                 formatter: function() {
+      //                   var first = this.series.data[0],
+      //                       last  = this.series.data[this.series.data.length - 1];
+      //                   if ((this.point.category === first.category && this.point.y === first.y) ||
+      //                       (this.point.category === last.category  && this.point.y === last.y)) {
+      //                     return this.point.y;
+      //                   }
+      //                   return "";
+      //                 }
+      //               },
+      //             },
+      // }
+      //       }
         },
         rangeselector: {
             height: 40
@@ -86,7 +132,7 @@ $(function () {
             name: 'Capacitance',
             type: 'line',
             yAxis: 1,
-            data: randArrayMaker(50,5,80),
+            data: data1,
             tooltip: {
                 valueSuffix: ' pF'
             }
@@ -95,7 +141,7 @@ $(function () {
             name: 'Voltage',
             type: 'line',
             yAxis: 2,
-            data: randArrayMaker(50,20,120),
+            data: data2,
             marker: {
                 enabled: false
             },
@@ -106,15 +152,15 @@ $(function () {
 
         }, {
             name: 'Temperature',
-            type: 'spline',
-            data: randArrayMaker(50,1,20),
+            type: 'line',
+            data: data3,
             tooltip: {
                 valueSuffix: ' °C'
             }
         }]
     });
 });
-
+console.log(dateArray(50));
 
 function randArrayMaker(length,volatility,constant) {
   data = []
@@ -124,4 +170,24 @@ function randArrayMaker(length,volatility,constant) {
         data.push(randNum)
   }
   return data
+}
+
+function randomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+}
+
+function dateArray(length) {
+  var dateArray = [];
+  for ( i = 0; i < length; i++ ) {
+    dateArray.push((randomDate(new Date(2000, 0, 1), new Date())).toString())
+  }
+  return dateArray
+}
+
+function newDataArray(array1, array2) {
+  result = [];
+  for ( var i = 0; i < array1.length; i++ ) {
+    result.push( [ array1[i], array2[i] ] );
+  }
+  return result
 }
